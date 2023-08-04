321fba65ee5ea1fb358238fbe88744beb0da3030
OusmBlueNinja <89956790+OusmBlueNinja@users.noreply.github.com>
2023-08-04 17:38:59 -0500

    Added base files for website
    added wiki, contact and about page's
    
    fixed Nav bar buttons and style.css
    
    added:
      Blocked Spam-Refresh Atack: useing ```openPage(evt, name)```

---
+++ script.js
@@ -0,0 +1,8 @@
+function openPage(evt, name) {
+    // NOTE SO SELF. make this work
+    var newLocation = "/" + name + ".html";
+    // Prevents Reload of page when spamming the same button.
+    if  ( !location.href.endsWith(newLocation) ) {
+        location.href = newLocation;
+    }
+}
