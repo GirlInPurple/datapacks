e21a870a184572d1d39a10fdcc4678e02df7d45c
OusmBlueNinja <spencer.conlon3@gmail.com>
2023-08-04 19:42:33 -0500

    new file:   assets/text-logo.png 	modified:   index.html 	modified:   script.js 	modified:   style.css 	modified:   wiki.html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openPage(evt, name) {
     // NOTE SO SELF. make this work
-    var newLocation = "/finalrepublic/" + name + ".html";
+    var newLocation = "/" + name + ".html";
     // Prevents Reload of page when spamming the same button.
     if  ( !location.href.endsWith(newLocation) ) {
         location.href = newLocation;
