3ada01713e448ca1148f073ff5a4d70fd77afade
The Color Blurple <turtlenerd603@gmail.com>
2023-09-17 17:26:38 -0400

    revision push

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 // no more openPage(), i fixed the bug :D
 
-// on script load
-const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-themeStylesheet.setAttribute('href', isDarkMode ? 'dark.css' : 'light.css');
+window.onload = function() {
+    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    themeStylesheet.setAttribute('href', isDarkMode ? 'dark.css' : 'light.css');
+}
+
+// is this function overbuilt? yes. am i going to rewrite it? ive already done that... twice
+function toggleTheme(toggleTo = 0) {
+    const themeStylesheet = document.getElementById('themeStylesheet');
+    const currentTheme = themeStylesheet.getAttribute('href');
+    const darkTheme = 'dark.css';
+    const lightTheme = 'light.css';
+
+    if (toggleTo == 0) {
+        // toggle to the opposite
+        if (currentTheme === darkTheme) {
+            themeStylesheet.setAttribute('href', lightTheme);
+        } else {
+            themeStylesheet.setAttribute('href', darkTheme);
+        }
+    } else if (toggleTo == "dark" || toggleTo == 'light') {
+        themeStylesheet.setAttribute('href', toggleTo + '.css');
+        console.log("using "+toggleTo+".css")
+    } else if (toggleTo == "system") {
+        systemTheme();
+        console.log("using system theme");
+    } else {
+        console.error("couldn't change theme | given value \"" + toggleTo + "\" not accepted.");
+    }
+}
+
+function checkCheckboxes(systemValue, darkValue, lightValue) {
+    document.getElementById('systemTheme').checked = systemValue;
+    document.getElementById('darkTheme').checked = darkValue;
+    document.getElementById('lightTheme').checked = lightValue;
+}
 
 function toggleElementVisibility(imageIdent) {
     var image = document.getElementById(imageIdent);
@@ -13,24 +45,6 @@ function toggleElementVisibility(imageIdent) {
     }
 }
 
-function toggleTheme() {
-    const themeStylesheet = document.getElementById('themeStylesheet');
-    const currentTheme = themeStylesheet.getAttribute('href');
-    const darkTheme = 'dark.css';
-    const lightTheme = 'light.css';
-
-    // Check the current theme and toggle to the opposite
-    if (currentTheme === darkTheme) {
-        themeStylesheet.setAttribute('href', lightTheme);
-        //document.getElementById("themeToggle").setAttribute("src", "https://cdn3.iconfinder.com/data/icons/camera-scene-mode-glyph-16px/16/night_mode_moon_scene_dark-512.png");
-        console.log("i highly respect people who use light mode, especially at night, that takes some dedication to do. i do not have that dedication.")
-    } else {
-        themeStylesheet.setAttribute('href', darkTheme);
-        //document.getElementById("themeToggle").setAttribute("src", "https://cdn1.iconfinder.com/data/icons/mix-ui/24/Sun_Brightness_Day_Light_Weather_Mode-512.png");
-    }
-}
-
-// what was the point of uinserttag()? was it a bug fix for something i dont understand? either way this seems to work just as well, if not better
 function insertTag(tag) {
     const searchInput = document.getElementById("searchInput");
     searchInput.value = searchInput.value + tag;
